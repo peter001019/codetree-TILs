0b971e13656e7b4aa735027e42bb27047c51e2b9
SeungWoo Han <122414373+peter001019@users.noreply.github.com>
2024-07-09 19:51:07 +0900

    Title: 문자에 따른 명령 2, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240709/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,18 @@
+x, y = 0, 0
+move = str(input())
+
+#동, 남, 서, 북
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
+dir = 3
+
+for s in move:
+    if s == 'F':
+        x += dx[dir]
+        y += dy[dir]
+    elif s == 'L':
+        dir = (4 + dir - 1) % 4
+    else:
+        dir = (dir + 1) % 4
+
+print(str(x) + " " + str(y))
