0103362d5f45f973181aacff9a8157093352d7e6
SeungWoo Han <122414373+peter001019@users.noreply.github.com>
2024-07-13 17:18:47 +0900

    Title: 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240713/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,17 @@
+n, t = map(int, input().split())
+list1 = list(map(int, input().split()))
+list2 = list(map(int, input().split()))
+
+for _ in range(t):
+    temp1 = list1[2]
+    temp2 = list2[2]
+
+    for i in range(n - 1, 0, -1):
+        list1[i] = list1[i - 1]
+        list2[i] = list2[i - 1]
+    
+    list1[0] = temp2
+    list2[0] = temp1
+    
+print(*list1)
+print(*list2)
